7b45b1675e767cb1be983a4f42286fe1be6ebe3a
davidle2810 <anhlq28102002@gmail.com>
2023-08-12 18:39:31 +0700

    update api

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ class Database:
         return "SUCCESS"
 
     #booking
-    def booking(self, info: models.Booking) -> str:
-        command = "EXEC sp_booking '%s', '%s', '%s', '%s', '%s', '%s'" % (info.ID, info.mentorID, info.menteeID, info.book_at, info.time, info.status)
+    def postBooking(self, info: models.Booking) -> str:
+        command = "EXEC sp_postBooking '%s', '%s', '%s', '%s', '%s', '%s'" % (info.ID, info.mentorID, info.menteeID, info.book_at, info.time, info.status)
         try:
             result = ""
             self.cursor.execute(command)
@@ -129,8 +129,8 @@ class Database:
     
     
     # get mentee booking
-    def getMenteeBooking(self, menteeid: str) -> list:
-        command = "EXEC sp_getMenteeBooking '%s'" % (menteeid)
+    def getListBookingMentee(self, menteeid: str) -> list:
+        command = "EXEC sp_getListBookingMentee '%s'" % (menteeid)
         try:
             self.cursor.execute(command)
         except:
@@ -140,9 +140,21 @@ class Database:
             result.append([x for x in i])
         return result
     
-    # put mentor booking (insert available sessions can be booked)
-    def putMentorBooking(self, booking:models.Booking) -> list:
-        command = "EXEC sp_putMentorBooking '%s', '%s', '%s', '%s'" % (booking.mentorID, booking.menteeID, booking.book_at, booking.time)
+        # get mentor booking
+    def getListBookingMentor(self, mentorid: str) -> list:
+        command = "EXEC sp_getListBookingMentor '%s'" % (mentorid)
+        try:
+            self.cursor.execute(command)
+        except:
+            return "SOME ERROR OCCUR"
+        result = []
+        for i in self.cursor:
+            result.append([x for x in i])
+        return result
+    
+    # put mentor booking (update sessions can be booked)
+    def putBooking(self, booking:models.Booking) -> list:
+        command = "EXEC sp_putBooking '%s', '%s', '%s', '%s'" % (booking.mentorID, booking.menteeID, booking.book_at, booking.time)
         try:
             self.cursor.execute(command)
             self.conn.commit()
@@ -181,48 +193,25 @@ async def searchResults(keyword:str):
     '''return list as a result of matching information'''
     return database.getSearchResult(keyword)
 
-bookings_db = []
-
 # API endpoint to create a booking
 @app.post("/bookings/", response_model=models.Booking)
 def create_booking(booking: models.Booking):
-    booking.id = str(1)
-    bookings_db.append(booking.dict())
-    return booking
+    return database.postBooking(booking)
 
 # API endpoint to get the list of bookings for a specific customer (mentee)
 @app.get("/bookings/customer/{customer_id}")
 def get_bookings_by_customer(customer_id: str):
-    customer_bookings = [booking for booking in bookings_db if booking["menteeID"] == customer_id]
-    if not customer_bookings:
-        raise HTTPException(status_code=404, detail="No bookings found for this customer")
-    return customer_bookings
+    return database.getListBookingMentee(customer_id)
 
 # API endpoint to get the list of bookings for a specific mentor (tutor)
 @app.get("/bookings/mentor/{mentor_id}")
 def get_bookings_by_mentor(mentor_id: str):
-    mentor_bookings = [booking for booking in bookings_db if booking["mentorID"] == mentor_id]
-    if not mentor_bookings:
-        raise HTTPException(status_code=404, detail="No bookings found for this mentor")
-    return mentor_bookings
-
-# API endpoint to get the details of a specific booking
-@app.get("/bookings/{booking_id}")
-def get_booking(booking_id: str):
-    booking = next((booking for booking in bookings_db if booking["ID"] == booking_id), None)
-    if booking is None:
-        raise HTTPException(status_code=404, detail="Booking not found")
-    return booking
+    return database.getListBookingMentor(mentor_id)
 
 # API endpoint to update the details of a specific booking by mentor
 @app.put("/bookings/{booking_id}", response_model=models.Booking)
-def update_booking_by_mentor(booking_id: str, updated_booking: models.Booking):
-    booking_index = next((index for index, booking in enumerate(bookings_db) if booking["ID"] == booking_id), None)
-    if booking_index is None:
-        raise HTTPException(status_code=404, detail="Booking not found")
-    updated_booking.id = booking_id
-    bookings_db[booking_index] = updated_booking.dict()
-    return bookings_db[booking_index]
+def update_booking_by_mentor(updated_booking: models.Booking):
+    return database.putBooking(updated_booking)
 
 
 #uvicorn main:app --host localhost --port 8000
